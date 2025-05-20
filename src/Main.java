c2f40d2970e5e51575922592e1e73caedcf701c4
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-20 11:56:09 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Integer amount = 100;//состояние счета
+        Integer updateAmount = 1800;//сумма пополнения
+        Integer barrierBonus = 1000;//Порог начисления бонусов
+        Integer costBonus = 100;//сумма за которую начисляем 1 балл
+        Integer bonus; // количество бонусов
+
+
+        System.out.println("Счет пополнен на " + updateAmount + " руб.");
+
+        if (updateAmount >= barrierBonus) {//Условным оператором проверяю, превысила ли сумма пополнения порог
+            bonus = updateAmount / costBonus; // для этих двух разных сценариев рассчитываю сумму бонуса
+            System.out.println("Начислено " + bonus + " бонусов, и вы выиграли автомобиль!!!");
+        } else {
+            bonus = 0;
+            System.out.println("Сумма пополнения меньше " + barrierBonus + " руб. Бонусы не начисляются.");
+        }
+
+        amount = amount + updateAmount + bonus;
+        System.out.println("Текущее состояние счета " + amount + " руб.");//Вывожу на экран
+
+    }
+}
